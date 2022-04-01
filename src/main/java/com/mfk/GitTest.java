79208aae3c3f35e87ab9457a7afca79f0892af93
mengfk <2532235767@qq.com>
2022-04-01 22:55:27 +0800

    my first commit

--- src/main/java/com/mfk/GitTest.java
+++ src/main/java/com/mfk/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("test git");
         System.out.println("版本二 222");
+        System.out.println("版三 333");
     }
 }
